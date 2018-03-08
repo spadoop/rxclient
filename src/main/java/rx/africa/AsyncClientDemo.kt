4483dff93492223c0fc4c6b6ac371b56bdee1466
Your Name <you@example.com>
2018-03-09 00:21:54 +0800

    hehe

--- src/main/java/rx/africa/AsyncClientDemo.kt
+++ src/main/java/rx/africa/AsyncClientDemo.kt
@@ -1,6 +1,9 @@
 package rx.africa
 
 import kotlinx.coroutines.experimental.future.await
+import org.apache.http.HttpResponse
+import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.nio.client.HttpAsyncClient
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 import java.util.concurrent.Future
@@ -27,4 +30,30 @@ interface FutureCallback<T> {
     fun completed(result: T)
     fun failed(ex: Exception)
     fun cancelled()
+}
+//fun HttpAsyncClient.execute(request: HttpUriRequest): CompletableFuture<HttpResponse> {
+//    val future = CompletableFuture<HttpResponse>()
+//
+//    this.execute(request, object : FutureCallback<HttpResponse> {
+//        override fun completed(result: HttpResponse) {
+//            future.complete(result)
+//        }
+//
+//        override fun cancelled() {
+//            future.cancel(false)
+//        }
+//
+//        override fun failed(ex: Exception) {
+//            future.completeExceptionally(ex)
+//        }
+//    })
+//
+//    return future
+//}
+fun HttpAsyncClient.execute(request: HttpUriRequest): CompletableFutureCallback<HttpResponse> {
+    val future: FutureCallback<HttpResponse> = CompletableFutureCallback(CompletableFuture<HttpResponse>())
+
+    this.execute(request, future)
+
+    return future
 }
