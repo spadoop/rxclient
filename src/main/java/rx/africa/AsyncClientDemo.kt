cebf6c72046bf236f954dc27c016a553267cf62b
spadoop <bachelorczy@hotmail.com>
2018-03-09 15:03:39 +0800

    Update AsyncClientDemo.kt

--- src/main/java/rx/africa/AsyncClientDemo.kt
+++ src/main/java/rx/africa/AsyncClientDemo.kt
@@ -1,15 +1,20 @@
 package org.apache.http.concurrent
 
-import kotlinx.coroutines.experimental.CancellableContinuation
-import kotlinx.coroutines.experimental.cancelFutureOnCompletion
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.future.await
-import kotlinx.coroutines.experimental.suspendCancellableCoroutine
+import org.apache.http.HttpEntity
 import org.apache.http.HttpResponse
+import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.entity.ContentType
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient
+import org.apache.http.impl.nio.client.HttpAsyncClients
 import org.apache.http.nio.client.HttpAsyncClient
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 import java.util.concurrent.Future
+
 
 class CompletableFutureCallback<T>(
         val completableFuture: CompletableFuture<T>
@@ -35,15 +40,15 @@ interface FutureCallback<T> {
     fun cancelled()
 }
 
-/* step 2
-fun HttpAsyncClient.execute(request: HttpUriRequest): CompletableFutureCallback<HttpResponse> {
-    val future: FutureCallback<HttpResponse> = CompletableFutureCallback(CompletableFuture<HttpResponse>())
+/* step 2*/
+//    fun HttpAsyncClient.execute(request: HttpUriRequest): CompletableFutureCallback<HttpResponse> {
+//        val future: FutureCallback<HttpResponse> = CompletableFutureCallback(CompletableFuture<HttpResponse>())
+//
+//        this.execute(request, future)
+//
+//        return future as CompletableFutureCallback<HttpResponse>
+//    }
 
-    this.execute(request, future)
-
-    return future as CompletableFutureCallback<HttpResponse>
-}
-*/
 
 /*step 3*/
 suspend fun HttpAsyncClient.execute(request: HttpUriRequest): HttpResponse {
@@ -52,9 +57,11 @@ suspend fun HttpAsyncClient.execute(request: HttpUriRequest): HttpResponse {
             override fun completed(result: HttpResponse) {
                 cont.resume(result)
             }
+
             override fun cancelled() {
                 // Nothing
             }
+
             override fun failed(ex: Exception) {
                 cont.resumeWithException(ex)
             }
@@ -64,22 +71,23 @@ suspend fun HttpAsyncClient.execute(request: HttpUriRequest): HttpResponse {
         Unit
     }
 }
-fun main(args: Array<String>) {
-    var c:HttpAsyncClient = DefaultHttpAsyncClient()
-    val request = HttpGet("http://www.apache.org/")
-    val future = c.execute(request, object : FutureCallback<HttpResponse> {
-        override fun completed(response: HttpResponse) {
-            println(request.requestLine.toString() + "->"
-                    + response.statusLine)
-        }
-        override fun failed(ex: Exception) {
-            ex.printStackTrace()
-        }
-        override fun cancelled() {
-        }
-    })
-    val response = future.get()
-    println("Response: " + response.statusLine)
-    println("Response1: " + EntityUtils.toString(response.entity))
-    println("Shutting down")
+
+fun main(args: Array<String>) =runBlocking<Unit>{
+    var ht:HttpEntity =  StringEntity(  "", ContentType.APPLICATION_JSON)
+    val job = async {
+        Thread.sleep(100L)
+        for(i in 0..999)getCount("http://localhost:8090/api")
+    }
+    println(System.currentTimeMillis())
+    println("Response: " +  job.await() )
+    println(System.currentTimeMillis())
+}
+suspend fun getCount(url: String): HttpResponse {
+
+    var httpclient:CloseableHttpAsyncClient = HttpAsyncClients.createDefault()
+    httpclient.start()
+    val request = HttpGet(url)
+    val r = httpclient./* calling extension */execute(request) /* kotlinx.coroutines extension await()*/
+    httpclient.close()
+    return r
 }
